beb8d4e2798cb10d55a02b3c3eb240e12624f609
KshitijNegi12 <23kshitij.negi@gmail.com>
2023-06-12 22:12:55 +0530

    changes in js, added feature to add names of players

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+//Player Data
+let p1=prompt("Enter first player name.");
+if(p1==null||p1==""){
+    p1='Player 1';
+}
+let p2=prompt("Enter second player name.");
+if(p2==null||p2==""){
+    p2='Player 2';
+}
+if(p1===p2){
+    p1=p1+"-1";
+    p2=p2+"-2";
+}
+
+//Music
 let music= new Audio("./music/play.mp3");
 let ko=new Audio("./music/ko.mp3");
 let press= new Audio("./music/press.mp3");
 let turn='X';
+document.querySelector('.info').innerHTML="Turn for "+p1+" ("+turn+")";
 var gameover=false;
-// check for draw
-var pressed=0;
+var pressed=0;  // Handle how many grid filled for Draw
+
 //change turn of player
 const change=() =>{
     if(turn==='X'){
@@ -17,7 +33,6 @@ const change=() =>{
 
 //Check win
 const iswin =() =>{
-    // getting all grids to check
     let boxes=document.getElementsByClassName('boxtext');
     let possibilities = [
         [0, 1, 2],
@@ -31,7 +46,14 @@ const iswin =() =>{
     ]
     possibilities.forEach(ele=>{
         if(boxes[ele[0]].innerHTML===boxes[ele[1]].innerHTML  && boxes[ele[1]].innerHTML===boxes[ele[2]].innerHTML && boxes[ele[0]].innerHTML!==""){
-            document.querySelector('.info').innerHTML=turn+" wins";
+            let p;
+            if(turn=='X'){
+                p=p1;
+            }
+            else{
+                p=p2;
+            }
+            document.querySelector('.info').innerHTML=p+" wins";
             ko.play();
             gameover=true;
             pressed=0;
@@ -43,9 +65,7 @@ const iswin =() =>{
 // Logic
 // all 9 box
 let boxes=document.getElementsByClassName('box');
-// getting every ele
 Array.from(boxes).forEach(ele=>{
-    //handle span msg
     let txt=ele.querySelector('.boxtext');
     // Adding Event listner of click
     ele.addEventListener('click',()=>{
@@ -58,7 +78,14 @@ Array.from(boxes).forEach(ele=>{
                 //change the turn for in page
                 if(!gameover){
                     let msg=document.getElementsByClassName('info');
-                    msg[0].innerHTML="Turn for "+turn;
+                    let p;
+                    if(turn=='X'){
+                        p=p1;
+                    }
+                    else{
+                        p=p2;
+                    }
+                    msg[0].innerHTML="Turn for "+p+" ("+turn+")";
                 }
                 pressed++;
                 if(pressed===9){
@@ -69,18 +96,42 @@ Array.from(boxes).forEach(ele=>{
     })
 })
 
+// reset button work
 reset.addEventListener('click', ()=>{
+    let k=prompt("Do you want to change name press (Y) or any other key to remain unchange. ");
+    if(k=='Y'||k=='y'){
+        p1=prompt("Enter first player name.");
+        if(p1==null||p1==""){
+            p1='Player 1';
+        }
+        p2=prompt("Enter second player name.");
+        if(p2==null||p2==""){
+            p2='Player 2';
+        }
+        if(p1==p2){
+            p1=p1+"-1";
+            p2=p2+"-2";
+        }
+    }
     let boxes=document.querySelectorAll('.boxtext');
     Array.from(boxes).forEach(ele=>{
         ele.innerHTML="";
     })
     turn='X';
     let msg=document.getElementsByClassName('info');
-    msg[0].innerHTML="Turn for "+turn;
+    let p;
+    if(turn=='X'){
+        p=p1;
+    }
+    else{
+        p=p2;
+    }
+    msg[0].innerHTML="Turn for "+p+" ("+turn+")";
     gameover=false;
     pressed=0;
 })
 
+// for audio play
 document.addEventListener('click',function(){
     music.play();
     this.documentElement.removeEventListener('click',this);
